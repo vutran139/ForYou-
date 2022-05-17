681a39ae88e53ea86ca6d786b8701a324bb4a683
vutran139 <txgv138@gmail.com>
2022-05-17 14:51:24 +0700

    first commit

---
+++ present.js
@@ -0,0 +1,46 @@
+
+var to = 'Friend !';
+var gift_url = 'https://www.facebook.com/nguyendungcodonnd';
+var gift_image_url = '279216614_419608033319259_6034852707489760862_n.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
